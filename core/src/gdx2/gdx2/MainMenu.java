dd98fcc4a536325ebf183565175cdb438c1a59c4
weild5554 <weild5554@FHC-IC0034102.wrdsb.ca>
2016-10-19 14:28:11 -0400

    MainMenu

---
+++ core/src/gdx2/gdx2/MainMenu.java
@@ -0,0 +1,84 @@
+package gdx2.gdx2;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+
+public class MainMenu extends InputAdapter implements Screen {
+        final Game1 game;
+	OrthographicCamera camera;
+        Texture SongOne, Exit;
+        private boolean isSong1, isExit;
+
+	public MainMenu(final Game1 _game) {
+		game = _game;
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+                SongOne = new Texture("SongOne.jpg");               
+                Exit = new Texture("Exit.jpg");
+
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		camera.update();
+		game.batch.setProjectionMatrix(camera.combined);
+                game.batch.begin();
+                game.batch.draw(SongOne, Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+                game.batch.draw(Exit, Gdx.graphics.getWidth() / 2, 100);
+		game.batch.end();
+
+		if (isSong1) { //if button pressed go to corresponding screen
+			game.setScreen(new SongOne(game));
+			dispose();
+		} if (isExit) {
+                     Gdx.app.exit(); 
+                }
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void show() {
+	}
+
+	@Override
+	public void hide() {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void dispose() {
+	} 
+        @Override
+        public boolean touchDown (int screenX, int screenY, int pointer, int button) {
+	if (button == Input.Buttons.LEFT && screenX < Gdx.graphics.getWidth() / 2 + 30 && 
+                screenX > Gdx.graphics.getWidth() / 2 - 30 && 
+                screenY < Gdx.graphics.getHeight() / 2 + 30 && 
+                screenY < Gdx.graphics.getHeight() / 2 - 30) {
+            isSong1 = true;
+        } else if (button == Input.Buttons.LEFT && screenX < Gdx.graphics.getWidth() / 2 + 30 && 
+                screenX > Gdx.graphics.getWidth() / 2 - 30 && 
+                screenY < 130 && screenY > 70) {	
+            isExit = true;
+        }
+            return true;
+	}
+}
